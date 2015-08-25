c4406a9c4d0eb6750ec8f492c019adf5b0ebe1c7
Artem Reshetov <art.reshetov39@gmail.com>
2015-08-25 15:47:06 +0200

    Full script parser

--- get.php
+++ get.php
@@ -1,126 +1,94 @@
 <?
-require "RollingCurlMini.php";
 require_once 'simple_html_dom.php';
+include_once('dHttp/Client.php');
+include_once('dHttp/Url.php');
+include_once('dHttp/Response.php');
 
 $start = microtime(true);
-$ch = curl_init();
-$url = "http://www.info-realty.ru/forum/messages/forum2/topic4880/message47783/?result=reply#message47783";
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-curl_setopt($ch, CURLOPT_HEADER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_REFERER, $url);
-curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-$output = curl_exec($ch);
 
-$output = mb_convert_encoding($output, 'utf-8', mb_detect_encoding($output));
+$urls = array(
+    "http://www.info-realty.ru/forum/messages/forum2/topic4880/message47783/?result=reply#message47783",
+    'http://vk.com/housemoscow?w=wall-77669604_1355%2Fall',
+    'http://www.retalk.ru/index.php?showtopic=6247&st=20&gopid=49601&#entry49601',
+    'http://regforum.ru/showthread.php?p=1727572#post1727572',
+    'http://vk.com/club43414712?w=wall-43414712_952%2Fall',
+    'http://forumproperty.ru/agenstva-nedvizhimosti-moskovskogo-regiona/147-kak-vybrat-horoshee-i-poryadochnoe-agentstvo-nedvizhimosti.html',
+    'http://mfbi.ru/showthread.php?1791-%D0%98%D0%B4%D0%B5%D0%B8-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D0%BD%D0%B8%D1%8F-%D0%B1%D0%B8%D0%B7%D0%BD%D0%B5%D1%81%D0%B0-%D0%B0%D0%BA%D1%82%D0%B8%D0%B2%D0%BE%D0%B2-%D0%B1%D0%B5%D0%B7-%D0%B4%D0%B5%D0%BD%D0%B5%D0%B3'
 
-$document = str_get_html($output);
-curl_close($ch);
-$i = 0;
+);
 
+$result = array();
 
-$data = str_get_html($document);
-$item['url'] = $url;
-$head = $data->find("meta");
-$temphead = array();
-foreach ($head as $key) {
-    $temphead = array_merge($temphead, $key->attr);
-    if(isset($temphead['name'])) {
-        $temphead['name'] === 'robots' ? $item['head'] = $temphead['content'] : '';
-    }
-}
-//$item['title'] = count($data->find('title')) ? $data->find('title', 0)->plaintext : '';
-
-if (count($data->find('a'))) {
-    foreach ($data->find('a') as $a) {
-
-        if(stripos($a->href, 'allmoscowoffices.ru'))
-        {
-            stripos($a->href, 'allmoscowoffices.ru')>12?$item['rd'][]=1:$item['rd'][]=0;
-            $item['href'][] = $a->href;
-            $item['fl'][] = $a->rel;
+$callback = function($document,$url,$code) {
+    $local_result['url'] = $url;
+    $local_result['live'] = true;
+    $local_result['nfl'] = false;
+    $local_result['nix'] = false;
+    $local_result['rd'] = false;
+    if($code ===200) {
+        $document = mb_convert_encoding($document, 'utf-8', mb_detect_encoding($document));
+        $data = str_get_html($document);
+        $item_head = false;
+        $head = $data->find("meta");
+        $temphead = array();
+        foreach ($head as $key) {
+            $temphead = array_merge($temphead, $key->attr);
+            if (isset($temphead['name'])) {
+                $temphead['name'] === 'robots' ? $item_head = $temphead['content'] : '';
+            }
         }
+        if ($item_head) {
+            $item_head = explode(",", $item_head);
+            foreach ($item_head as $key) {
+                $key = trim($key);
+                $key == "nofollow" ? $local_result['nfl'] = true : '';
+                $key == "noindex" ? $local_result['nix'] = true : '';
+            }
+        };
+
+        if (count($data->find('a'))) {
+            $hook = false;
+            foreach ($data->find('a') as $a) {
+
+                if (stripos($a->href, 'allmoscowoffices.ru')) {
+                    stripos($a->href, 'allmoscowoffices.ru') > 12 ? $local_result['rd'] = true : $local_result['rd'] = false;
+                    $a->rel == !"nofollow" && !($local_result['nfl']) ? $local_result['nfl'] = false : $local_result['nfl'] = true;
+                    $hook = true;
+                }
+            }
+            $hook?$local_result['live'] = true:$local_result['live'] = false;
+        }
+        $data->clear();
+        unset($data, $head);
     }
+    else{
+        $local_result['live'] = false;
+    }
+    return $local_result;
+};
+
+$multi = new dHttp\Client();
+
+foreach ($urls as $url) {
+
+    $resp_once[] = new dHttp\Client($url, array(
+        CURLOPT_SSL_VERIFYPEER => FALSE,
+        CURLOPT_HEADER => TRUE,
+        CURLOPT_FOLLOWLOCATION=>TRUE,
+        CURLOPT_IPRESOLVE=>'CURL_IPRESOLVE_V4',
+        CURLOPT_RETURNTRANSFER=>TRUE
+    ));
 }
-$data->clear();
-unset($data, $head);
 
-echo '<pre>';
-print_r($item);
-echo '</pre>';
+$response_array = $multi->multi($resp_once);
 
+foreach($response_array as $item) {
+    $result[] = $callback($item->getRaw(),$item->getUrl(),$item->getCode());
 
-//-------------------------------------------------------------------
-// для подсчета времени выполнения
-//$start = microtime(true);
-//
-//$urls = array(
-//    'http://www.mamainfo.ru/forum/viewtopic.php?p=12141#12141',
-//    'http://nedvigimost.bbok.ru/viewtopic.php?id=987#p1476 ',
-//    'http://ruall.com/biznes-idei-i-sozdanie-novogo-biznesa/5879-kakim-biznesom-zanyatsya/Page-4.html#6713'
-//);
-//
-///*
-//    Функция возвращает массив:
-//    название марки авто, количество объявлений, ссылка
-//*/
-//
-//function callback($response, $info, $request) {
-//    // Обработка результатов
-////    $document = phpQuery::newDocumentHTML($response);
-////    echo '<pre>';
-////    print_r($document);
-////    echo '</pre>';
-//}
-//
-//$rc = new RollingCurlMini("callback");
-//$rc->window_size = 20; // Количество одновременных соединений
-//
-//foreach ($urls as $url) {
-//    $rc->get($url); // Формируем очередь запросов
-//}
-//
-//$rc->execute(); // Запускаем
-
-//function get_marks()
-//{
-//    $url = 'http://www.mamainfo.ru/forum/viewtopic.php?p=12141#12141';
-//
-//    $html = file_get_contents($url);
-//    $document = phpQuery::newDocumentHTML($html);
-////    $document = phpQuery::newDocumentXML($html);
-//    $hentry['name'] = $document->find('a')->text();
-//    $links = $document->find('a');
-//    foreach($links as $key){
-//        $links_arr[] = pq($key)->attr('href');
-//    }
-// $hentry['url'] = $document->find('a');
-//  phpQuery::newDocument($html);
-
-// проходим по li элементам с классом .marks-col-item
-//    foreach (pq('li.marks-col-item') as $mark){
-//        // ищем название марки (текст ссылки)
-//        $mark_name = trim(pq($mark)->find('a')->text());
-//        $marks[$mark_name]['mark'] = $mark_name;
-//        // ищем кол-во объявлений в span элементе с классом .marks-col-c
-//        $marks[$mark_name]['count'] = pq($mark)->find('span.marks-col-c')->text();
-//        // ищем адрес ссылки, и склеиваем с доменом в переменной $url
-//        $marks[$mark_name]['url'] = $url.pq($mark)->find('a')->attr('href');
-//    }
-//
-//    unset($html);
-
-//    return $links_arr;
-//}
-
-// выполняем функцию
-//$marks_array = get_marks();
-
-// выводим массив на экран
+}
+echo json_encode($result);
 //echo '<pre>';
-//print_r($marks_array);
+//var_dump($result);
 //echo '</pre>';
-
-echo '<strong>Время выполнения скрипта: ' . (microtime(true) - $start) . ' сек.</strong>';
+//
+//echo '<strong>Время выполнения скрипта: ' . (microtime(true) - $start) . ' сек.</strong>';
--- js/app.js
+++ js/app.js
@@ -1,35 +1,23 @@
-//function getXmlHttp(){
-//    var xmlhttp;
-//    try {
-//        xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
-//    } catch (e) {
-//        try {
-//            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-//        } catch (E) {
-//            xmlhttp = false;
-//        }
-//    }
-//    if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
-//        xmlhttp = new XMLHttpRequest();
-//    }
-//    return xmlhttp;
-//}
+var links=[
+    "http://www.info-realty.ru/forum/messages/forum2/topic4880/message47783/?result=reply#message47783",
+    'http://vk.com/housemoscow?w=wall-77669604_1355%2Fall',
+    'http://www.retalk.ru/index.php?showtopic=6247&st=20&gopid=49601&#entry49601',
+    'http://regforum.ru/showthread.php?p=1727572#post1727572',
+    'http://vk.com/club43414712?w=wall-43414712_952%2Fall',
+    'http://forumproperty.ru/agenstva-nedvizhimosti-moskovskogo-regiona/147-kak-vybrat-horoshee-i-poryadochnoe-agentstvo-nedvizhimosti.html',
+    'http://mfbi.ru/showthread.php?1791-%D0%98%D0%B4%D0%B5%D0%B8-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D0%BD%D0%B8%D1%8F-%D0%B1%D0%B8%D0%B7%D0%BD%D0%B5%D1%81%D0%B0-%D0%B0%D0%BA%D1%82%D0%B8%D0%B2%D0%BE%D0%B2-%D0%B1%D0%B5%D0%B7-%D0%B4%D0%B5%D0%BD%D0%B5%D0%B3'
+];
 
+var result;
 $(document).ready(function(){
     $.ajax({
         url: "get.php",
+        data: links,
         success: function(data) {
             console.log(data);
+            result = data;
         }
     });
-//    var xmlhttp = getXmlHttp()
-//    xmlhttp.open('GET', 'http://homemade.ru/forum/viewtopic.php?p=25475#25475', true);
-//    xmlhttp.onreadystatechange = function() {
-//        if (xmlhttp.readyState == 4) {
-//            if(xmlhttp.status == 200) {
-//                alert(xmlhttp.responseText);
-//            }
-//        }
-//    };
-//    xmlhttp.send(null);
 });
+
+console.log(result);
