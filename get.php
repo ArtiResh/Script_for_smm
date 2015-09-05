5bf3f6f7d064da89776e0dbd06d5ca0de9afb3cd
Artem Reshetov <art.reshetov39@gmail.com>
2015-09-05 12:04:02 +0200

    phpQuery

--- get.php
+++ get.php
@@ -1,8 +1,8 @@
 <?
-require_once 'sources/simple_html_dom.php';
 include_once('sources/dHttp/Client.php');
 include_once('sources/dHttp/Url.php');
 include_once('sources/dHttp/Response.php');
+include_once('sources/phpQuery.php');
 
 $params = json_decode(file_get_contents('php://input'));
 
@@ -10,75 +10,61 @@ $params = json_decode(file_get_contents('php://input'));
 
 $result = array();
 
-$callback = function($document,$url,$code,$target) {
+$callback = function ($document, $url, $code, $target) {
     $local_result['url'] = $url;
     $local_result['live'] = true;
     $local_result['nfl'] = '';
     $local_result['nix'] = '';
     $local_result['rd'] = '';
     $local_result['clear'] = false;
-    if($code === 200) {
-//        $document = mb_convert_encoding($document, 'utf-8', mb_detect_encoding($document));
-        $data = str_get_html($document);
-        $item_head = false;
-        $head = $data->find("meta");
-        $temphead = array();
-        foreach ($head as $key) {
-            $temphead = array_merge($temphead, $key->attr);
-            if (isset($temphead['name'])) {
-                $temphead['name'] === 'robots' ? $item_head = $temphead['content'] : '';
+    if ($code === 200){
+
+        $tags = get_meta_tags($url);
+
+        foreach ($tags as $tag) {
+            $tag = explode(',',$tag);
+            foreach ($tag as $t){
+                $t = trim(mb_strtolower($t));
+                $t == "nofollow" ? $local_result['nfl'] = 'nf' : '';
+                $t == "noindex" ? $local_result['nix'] = 'ni' : '';
             }
         }
-        if ($item_head) {
-            $item_head = explode(",", $item_head);
-            foreach ($item_head as $key) {
-                $key = trim($key);
-                $key == "nofollow" ? $local_result['nfl'] = 'nf' : '';
-                $key == "noindex" ? $local_result['nix'] = 'ni' : '';
-            }
-        };
 
+        $data =  phpQuery::newDocument($document);
+        $hook = false;
         if (count($data->find('a'))) {
-            $hook = false;
-            $hashed = false;
-            foreach ($data->find('a') as $a) {
-                $hashed = false;
-                if (stripos($a->href, $target)) {
-                    stripos($a->href, $target) > 12 ? $local_result['rd'] = 'rd' : $local_result['rd'] = '';
-                    $a->rel != "nofollow" && !($local_result['nfl']==='nf') ? $local_result['nfl'] = '' : $local_result['nfl'] = 'nf';
+            foreach (pq('a') as $a) {
+                if (stripos(pq($a)->attr('href'), $target)) {
+                    stripos(pq($a)->attr('href'), $target) > 12 ? $local_result['rd'] = 'rd' : $local_result['rd'] = '';
+                    pq($a)->attr('rel') != "nofollow" && !($local_result['nfl'] === 'nf') ? $local_result['nfl'] = '' : $local_result['nfl'] = 'nf';
                     $hook = true;
-                }
-                else{
-                    $hashed = true;
-                }
-                if(stripos($a->innertext, $target)&&($hashed))
-                {
-                    $a->rel != "nofollow" && !($local_result['nfl']==='nf') ? $local_result['nfl'] = '' : $local_result['nfl'] = 'nf';
-                    $local_result['rd'] = 'rd';
-                    $hook = true;
+                } else {
+                    if (stripos(pq($a)->text(), $target)) {
+                        pq($a)->attr('rel') != "nofollow" && !($local_result['nfl'] === 'nf') ? $local_result['nfl'] = '' : $local_result['nfl'] = 'nf';
+                        $local_result['rd'] = 'rd';
+                        $hook = true;
+                    }
                 }
             }
-            $hook?$local_result['live'] = true:$local_result['live'] = false;
         }
         if(count($data->find('span'))){
-            foreach ($data->find('span') as $span) {
-                if (stripos($span->onclick, $target)) {
+            foreach (pq('span') as $span) {
+                if (stripos(pq($span)->attr('onclick'), $target)) {
                     $local_result['rd'] = 'rd';
-                    $local_result['live'] = true;
+                    $hook = true;
                 }
             }
         }
         if (count($data->find('noindex'))){
-            foreach ($data->find('noindex') as $noindex) {
-                if (stripos($noindex->innertext, $target))$local_result['nix'] = 'ni';
+            foreach (pq('noindex') as $noindex) {
+                if (stripos(pq($noindex)->text(), $target))$local_result['nix'] = 'ni';
             }
 
         }
         ($local_result['rd'] != 'rd'&&$local_result['nix'] != 'ni'&&$local_result['nfl'] != 'nf')?$local_result['clear'] = true:'';
-        $data->clear();
-        unset($data, $head);
-    }
-    else{
+        $hook?$local_result['live'] = true:$local_result['live'] = false;
+        unset($data);
+    } else {
         $local_result['live'] = false;
     }
     return $local_result;
@@ -88,7 +74,7 @@ $multi = new dHttp\Client();
 $used_links = array();
 foreach ($params->links as $url) {
     $url = trim($url);
-    if(!in_array($url,$used_links)) {
+    if (!in_array($url, $used_links)) {
         $resp_once[] = new dHttp\Client($url, array(
             CURLOPT_SSL_VERIFYPEER => FALSE,
             CURLOPT_HEADER => TRUE,
@@ -104,8 +90,8 @@ foreach ($params->links as $url) {
 
 $response_array = $multi->multi($resp_once);
 
-foreach($response_array as $item) {
-    $result[] = $callback($item->getRaw(),$item->getUrl(),$item->getCode(),$params->target);
+foreach ($response_array as $item) {
+    $result[] = $callback($item->getRaw(), $item->getUrl(), $item->getCode(), $params->target);
 
 }
 echo json_encode($result);
